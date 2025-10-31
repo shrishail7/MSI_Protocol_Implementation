e9fb8c7e6a15c1422fb932c5f9fa0f15d8bcad97
Shrishail <shrishaildolle7878@gmail.com>
2025-11-01 00:21:26 +0530

    added read and write logic for L1_cche

--- L1_cache.v
+++ L1_cache.v
@@ -19,7 +19,7 @@ module L1_cache #(
     // ========== read operation ports ==========
     input wire [$clog2(No_Sets)-1:0] rd_set_idx,
     input wire rd_en,
-    
+
     // tag
     output wire [Tag_Bits-1:0] rd_tag_way0,
     output wire [Tag_Bits-1:1] rd_tag_way1,
@@ -55,5 +55,94 @@ module L1_cache #(
     input wire [1:0] wr_msi
 
 );
+
+    // ========== Cache Storage Arrays ==========
+    reg [Tag_Bits-1:0] tag_array [0:No_Sets-1][0:No_Ways-1];
+    reg [Line_Bytes*8-1:0] data_array [0:No_Sets-1][0:No_Ways-1];
+    reg valid_array [0:No_Sets-1][0:No_Ways-1];
+    reg dirty_array [0:No_Sets-1][0:No_Ways-1];
+    reg lru_array [0:No_Sets-1];
+
+    // ========== MSI State Arrays ==========
+    reg [1:0] msi_state_array [0:No_Sets-1][0:No_Ways-1];
+
+
+    // ========== Initialisation all memory ==========
+
+    integer  i,j ;
+
+    initial 
+        begin
+          for(i=0;i<No_Sets;i=i+1)
+            begin
+                for(j=0;j<No_Ways;j=j+1)
+                    begin
+                        tag_array[i][j]=0;
+                        data_array[i][j]=0;
+                        valid_array[i][j]=0;
+                        dirty_array[i][j]=0;
+                        msi_state_array[i][j] = `MSI_INVALID;
+                    end
+                    lru_array[i]=0;
+            end  
+        end
+
+    // ========== Reset logic ==========
+    //    i=0,j=0;
+    always @(posedge clk) 
+        begin
+            if(reset)
+                begin
+                    for(i=0;i<No_Sets;i=i+1)
+                        begin
+                            for(j=0;j<No_Ways;j=j+1)
+                                begin
+                                    tag_array[i][j]<=0;
+                                    data_array[i][j]<=0;
+                                end 
+                        end
+                end
+        end
+    // ========== Write Logic (Synchronous) ==========
     
+    always @(posedge clk ) begin
+        if(!reset && wr_en)
+            begin
+                tag_array[wr_set_idx][wr_way]<= wr_tag;
+                data_array[wr_set_idx][wr_way]<=wr_data;
+                valid_array[wr_set_idx][wr_way]<=wr_valid;
+                dirty_array[wr_set_idx][wr_way]<=wr_dirty;
+                msi_state_array[wr_set_idx][wr_way]<=wr_msi;
+                lru_array[wr_set_idx]<=wr_lru;
+            end
+    end
+
+
+    // ===== Read Logic (Combinational) =====
+    // tag
+    assign rd_tag_way0 = tag_array[rd_set_idx][0];
+    assign rd_tag_way1 = tag_array[rd_set_idx][1];
+
+    //data
+    assign rd_data_way0 = data_array[rd_set_idx][0];
+    assign rd_data_way1 = data_array[rd_set_idx][1];
+
+    //valid
+    assign rd_valid_way0 = valid_array[rd_set_idx][0];
+    assign rd_valid_way1 = valid_array[rd_set_idx][1];
+
+    //dirty
+    assign rd_dirty_way0 = dirty_array[rd_set_idx][0];
+    assign rd_dirty_way1 = rd_dirty_way1[rd_set_idx][1];
+
+    //lru
+    assign rd_lru_bit = lru_array[rd_set_idx];
+
+    assign rd_msi_way0 = msi_state_array[rd_set_idx][0];
+    assign rd_msi_way1 = msi_state_array[rd_set_idx][1];
+
+
+
+
+
 endmodule
