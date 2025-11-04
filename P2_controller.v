ebac7bd2ce62c29ded0a0f078f3e97eaafa1095f
Shrishail <shrishaildolle7878@gmail.com>
2025-11-04 07:30:19 +0530

    added latency for all possible hit/miss to P2 controller

--- P2_controller.v
+++ P2_controller.v
@@ -107,196 +107,263 @@ module P2_controller (
 
 
     always @(posedge clk ) 
-    begin
-        if(reset)
-            begin
-                current_state <= STATE_IDLE;
-                pending_req_type <= `PR_IDLE;
-                pending_addr <= 16'b0;
-                pending_data <= 128'b0;
-                bus_req_valid <= 1'b0;
-                l1_wr_en <= 1'b0;
-                proc_resp_valid <= 1'b0;
+        begin
+            if(reset)
+                begin
+                    current_state <= STATE_IDLE;
+                    pending_req_type <= `PR_IDLE;
+                    pending_addr <= 16'b0;
+                    pending_data <= 128'b0;
+                    bus_req_valid <= 1'b0;
+                    l1_wr_en <= 1'b0;
+                    proc_resp_valid <= 1'b0;
 
-                // ===== latency register =====
-                latency_counter <= 3'b0;
-                latency_state <= `LATENCY_IDLE;
-                latency_responce_pending <= 1'b0;
-                pending_read_data <= 128'b0;
-                response_source <= 3'b0;
-            end
-        else
-            current_state <= next_state ;
-    end
+                    // ===== latency register =====
+                    latency_counter <= 3'b0;
+                    latency_state <= `LATENCY_IDLE;
+                    latency_responce_pending <= 1'b0;
+                    pending_read_data <= 128'b0;
+                    response_source <= 3'b0;
+                end
+            else
+                current_state <= next_state ;
+        end
 
 
     always @(*) 
-    begin
-        next_state = current_state ;
-        bus_req_valid = 1'b0;
-        l1_wr_en = 1'b0;
-        proc_resp_valid = 1'b0 ;
-        l1_rd_set_idx = pending_set_idx;
-        hit_valid = 1'b0;
-        hit_way = 1'b0;
-
-        if(l1_rd_valid_way0 && (l1_rd_tag_way0 == pending_set_idx))
         begin
-            hit_valid = 1'b1;
+            next_state = current_state ;
+            bus_req_valid = 1'b0;
+            l1_wr_en = 1'b0;
+            proc_resp_valid = 1'b0 ;
+            l1_rd_set_idx = pending_set_idx;
+            hit_valid = 1'b0;
             hit_way = 1'b0;
-            hit_msi_state = l1_rd_msi_way0;
-        end
-        else if(l1_rd_valid_way1 && (l1_rd_tag_way1 == pending_set_idx))
-        begin
-            hit_valid = 1'b1;
-            hit_way = 1'b1;
-            hit_msi_state = l1_rd_msi_way1;
-        end
 
-    case (current_state)
-
-        STATE_IDLE :
+            if(l1_rd_valid_way0 && (l1_rd_tag_way0 == pending_set_idx))
             begin
-                if(proc_req_type!=`PR_IDLE)
-                begin
-                    pending_req_type <= proc_req_type;
-                    pending_addr <= proc_req_addr ;
-                    pending_data <= proc_write_data;
-                    pending_set_idx <= proc_req_addr[8:4];
-                    pending_tag <= proc_req_addr[15:9];
+                hit_valid = 1'b1;
+                hit_way = 1'b0;
+                hit_msi_state = l1_rd_msi_way0;
+            end
+            else if(l1_rd_valid_way1 && (l1_rd_tag_way1 == pending_set_idx))
+            begin
+                hit_valid = 1'b1;
+                hit_way = 1'b1;
+                hit_msi_state = l1_rd_msi_way1;
+            end
 
-                    if(proc_req_type == `PR_RD)
+        case (current_state)
+
+            STATE_IDLE :
+                begin
+                    if(proc_req_type!=`PR_IDLE)
                     begin
-                        next_state = STATE_PROC_READ;
-                    end        
-                    else if(proc_req_type == `PR_WR)
-                    begin
-                        next_state = STATE_PROC_WRITE;
+                        pending_req_type <= proc_req_type;
+                        pending_addr <= proc_req_addr ;
+                        pending_data <= proc_write_data;
+                        pending_set_idx <= proc_req_addr[8:4];
+                        pending_tag <= proc_req_addr[15:9];
+
+                        if(proc_req_type == `PR_RD)
+                        begin
+                            next_state = STATE_PROC_READ;
+                        end        
+                        else if(proc_req_type == `PR_WR)
+                        begin
+                            next_state = STATE_PROC_WRITE;
+                        end
                     end
                 end
-            end
 
 
-        STATE_PROC_READ:
-            begin
-                l1_rd_set_idx = pending_set_idx;
-
-                // latency expired
-                if(hit_valid && latency_state==`LATENCY_COMPLETE)
+            STATE_PROC_READ:
                 begin
-                    proc_resp_valid = 1'b1;
-                    proc_resp_hit = 1'b1;
-                    proc_read_data = pending_read_data;
-                    next_state = STATE_IDLE;
-                end 
-                else if(!hit_valid)
-                begin
-                    if(!bus_req_grant)
+                    l1_rd_set_idx = pending_set_idx;
+
+                    // latency expired
+                    if(hit_valid && latency_state==`LATENCY_COMPLETE)
                     begin
-                        bus_req_valid = 1'b1;
-                        bus_req_type = `BUS_RD;
-                        bus_req_addr = pending_addr ;
-                        bus_req_data = 128'd0;
-                        next_state = STATE_BUS_WAIT;
+                        proc_resp_valid = 1'b1;
+                        proc_resp_hit = 1'b1;
+                        proc_read_data = pending_read_data;
+                        next_state = STATE_IDLE;
                     end 
-                end
-            end
-
-        STATE_PROC_WRITE:
-            begin
-                l1_rd_set_idx = pending_set_idx ;
-
-                if(hit_valid && hit_msi_state == `MSI_MODIFIED)
-                begin
-                    l1_wr_en = 1'b1;
-                    l1_wr_set_idx = pending_set_idx ;
-                    l1_wr_way = hit_way ;
-                    l1_wr_tag = pending_tag;
-                    l1_wr_data = pending_data ; 
-                    l1_wr_valid = 1'b1;
-                    l1_wr_msi = `MSI_MODIFIED;
-                    l1_wr_dirty = 1'b1;
-                    l1_wr_lru = l1_rd_lru_bit;
-                    proc_resp_valid = 1'b1;
-                    proc_resp_hit = 1'b1;
-                    next_state = STATE_IDLE;
-                end
-                else if(hit_valid && hit_msi_state == `MSI_SHARED)
-                begin
-                    if(!bus_req_grant)
+                    else if(!hit_valid)
                     begin
-                        bus_req_valid = 1'b1;
-                        bus_req_type = `BUS_UPGR ;
-                        bus_req_addr = pending_addr;
-                        bus_req_data = 128'b0;
-                        next_state = STATE_BUS_WAIT;
-                    end 
+                        if(!bus_req_grant)
+                        begin
+                            bus_req_valid = 1'b1;
+                            bus_req_type = `BUS_RD;
+                            bus_req_addr = pending_addr ;
+                            bus_req_data = 128'd0;
+                            next_state = STATE_BUS_WAIT;
+                        end 
+                    end
                 end
-                else
-                begin
-                    if(!bus_req_grant)
-                    begin
-                        bus_req_valid= 1'b1;
-                        bus_req_type = `BUS_RDX;
-                        bus_req_addr = pending_addr;
-                        bus_req_data = 128'b0;
-                        next_state = STATE_BUS_WAIT;
-                    end 
-                end
-            end
 
-        STATE_BUS_WAIT :
-            begin
-                if(L2_resp_valid)
+            STATE_PROC_WRITE:
+                begin
+                    l1_rd_set_idx = pending_set_idx ;
+
+                    if(hit_valid && hit_msi_state == `MSI_MODIFIED)
                     begin
-                        l1_wr_en=1'b1;
-                        l1_wr_set_idx = pending_set_idx;
-                        l1_wr_way = l1_rd_lru_bit ? 1'b1 : 1'b0;
+                        l1_wr_en = 1'b1;
+                        l1_wr_set_idx = pending_set_idx ;
+                        l1_wr_way = hit_way ;
                         l1_wr_tag = pending_tag;
-                        l1_wr_data = L2_resp_data;
+                        l1_wr_data = pending_data ; 
                         l1_wr_valid = 1'b1;
-                        l1_wr_dirty = 1'b0;
-
-                        if(pending_req_type == `PR_RD)
-                        begin
-                            l1_wr_msi = `MSI_SHARED;
-                            l1_wr_dirty = 1'b0;
-                        end
-                        else 
-                        begin
-                            l1_wr_msi = `MSI_MODIFIED;
-                            l1_wr_dirty = 1'b1;
-                        end
-
+                        l1_wr_msi = `MSI_MODIFIED;
+                        l1_wr_dirty = 1'b1;
                         l1_wr_lru = l1_rd_lru_bit;
-                        proc_resp_valid = 1'b0; // dont respond immediately
-                        proc_resp_hit = 1'b0;
+                        proc_resp_valid = 1'b1;
+                        proc_resp_hit = 1'b1;
                         next_state = STATE_IDLE;
                     end
-            end
-
-        STATE_SNOOP :
-            begin
-                if(bus_valid && bus_addr[15:9]==pending_tag)
-                begin
-                    if(bus_msg_type == `BUS_INV)
+                    else if(hit_valid && hit_msi_state == `MSI_SHARED)
+                    begin
+                        if(!bus_req_grant)
                         begin
-                            if(hit_valid)
-                                begin
-                                    l1_wr_en = 1'b1;
-                                    l1_wr_set_idx = pending_set_idx;
-                                    l1_wr_way = hit_way;
-                                    l1_wr_msi = `MSI_INVALID;
-                                    l1_wr_valid = 1'b0;
-                                end 
+                            bus_req_valid = 1'b1;
+                            bus_req_type = `BUS_UPGR ;
+                            bus_req_addr = pending_addr;
+                            bus_req_data = 128'b0;
+                            next_state = STATE_BUS_WAIT;
+                        end 
+                    end
+                    else
+                    begin
+                        if(!bus_req_grant)
+                        begin
+                            bus_req_valid= 1'b1;
+                            bus_req_type = `BUS_RDX;
+                            bus_req_addr = pending_addr;
+                            bus_req_data = 128'b0;
+                            next_state = STATE_BUS_WAIT;
+                        end 
+                    end
+                end
+
+            STATE_BUS_WAIT :
+                begin
+                    if(L2_resp_valid)
+                        begin
+                            l1_wr_en=1'b1;
+                            l1_wr_set_idx = pending_set_idx;
+                            l1_wr_way = l1_rd_lru_bit ? 1'b1 : 1'b0;
+                            l1_wr_tag = pending_tag;
+                            l1_wr_data = L2_resp_data;
+                            l1_wr_valid = 1'b1;
+                            l1_wr_dirty = 1'b0;
+
+                            if(pending_req_type == `PR_RD)
+                            begin
+                                l1_wr_msi = `MSI_SHARED;
+                                l1_wr_dirty = 1'b0;
+                            end
+                            else 
+                            begin
+                                l1_wr_msi = `MSI_MODIFIED;
+                                l1_wr_dirty = 1'b1;
+                            end
+
+                            l1_wr_lru = l1_rd_lru_bit;
+                            proc_resp_valid = 1'b0; // dont respond immediately
+                            proc_resp_hit = 1'b0;
+                            next_state = STATE_IDLE;
                         end
                 end
-                next_state = STATE_IDLE;
-            end
-    endcase
-    end
+
+            STATE_SNOOP :
+                begin
+                    if(bus_valid && bus_addr[15:9]==pending_tag)
+                    begin
+                        if(bus_msg_type == `BUS_INV)
+                            begin
+                                if(hit_valid)
+                                    begin
+                                        l1_wr_en = 1'b1;
+                                        l1_wr_set_idx = pending_set_idx;
+                                        l1_wr_way = hit_way;
+                                        l1_wr_msi = `MSI_INVALID;
+                                        l1_wr_valid = 1'b0;
+                                    end 
+                            end
+                    end
+                    next_state = STATE_IDLE;
+                end
+        endcase
+        end
+
+// ========== NEW: LATENCY COUNTER FSM ==========
 
 
+    always @(posedge clk ) 
+        begin
+            if(reset)
+                begin
+                    latency_counter <= 3'b0;
+                    latency_state <= `LATENCY_IDLE;
+                    latency_responce_pending <= 1'b0;
+                    pending_read_data <= 128'b0;
+                    response_source <= 3'b0;
+                end
+            else
+                begin
+                    // ===== SCENARIO 1: L1 HIT (3 CYCLE LATENCY) =====
+                    if(current_state == STATE_PROC_READ && hit_valid &&latency_state == `LATENCY_IDLE)
+                        begin
+                            latency_state <= `LATENCY_ACTIVE;
+                            latency_counter <= `LATENCY_L1_HIT - 1;
+                            latency_responce_pending <= 1'b1 ;
+                            response_source <= `LATENCY_SOURCE_L1; 
 
-    
+                            if(hit_way==1'b0)
+                                pending_read_data <= l1_rd_data_way0;
+                            else 
+                                pending_read_data <= l1_rd_data_way1;
+                        end
+
+                    // ===== SCENARIO 2: L2 HIT (5 CYCLE LATENCY) =====
+                    if(current_state == STATE_BUS_WAIT && L2_resp_valid && L2_hit_valid && latency_state == `LATENCY_IDLE)
+                        begin
+                            latency_state <= `LATENCY_ACTIVE;
+                            latency_counter <= `LATENCY_L2_HIT -1;
+                            latency_responce_pending <= 1'b1;
+                            response_source <= `LATENCY_SOURCE_L2;
+                            pending_read_data <= L2_resp_data;
+                        end
+
+                    // ===== SCENARIO 3: MEMORY HIT (7 CYCLE LATENCY) =====
+                    if(current_state == STATE_BUS_WAIT && L2_resp_valid && !L2_hit_valid && latency_state == `LATENCY_IDLE)
+                         begin
+                            latency_state <= `LATENCY_ACTIVE;
+                            latency_counter <= `LATENCY_MEMORY_HIT -1 ;
+                            latency_responce_pending <= 1'b1;
+                            response_source <= `LATENCY_SOURCE_MEM;
+                            pending_read_data <= L2_resp_data;
+                         end
+                    
+                    // ===== DECREMENT LATENCY COUNTER =====
+                    if(latency_state == `LATENCY_ACTIVE)
+                        begin
+                            if(latency_counter >3'd0)
+                                begin
+                                    latency_counter<= latency_counter-3'd1;
+                                end 
+                            else
+                                begin
+                                    latency_state <= `LATENCY_COMPLETE;
+                                end
+                        end
+
+                    // ===== CLEAR LATENCY AFTER RESPONSE =====
+                    if(latency_state == `LATENCY_COMPLETE && proc_req_type == `PR_TDLE)
+                        begin
+                            latency_responce_pending <= 1'b0;
+                            latency_state <= `LATENCY_IDLE;
+                        end
+                end
+        end    
 endmodule
